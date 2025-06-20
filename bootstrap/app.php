daf649ba1a6f4ea90d67ab69301c98eb8910eddc
dany214 <dmeza2021@gmail.com>
2025-06-20 00:08:32 -0500

    Fix formatting of CORS middleware usage in application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->use([Illuminate\Http\Middleware\HandleCors::class]);
+        $middleware->use([\Illuminate\Http\Middleware\HandleCors::class]);
         $middleware->alias([
             'admin' => \App\Http\Middleware\AuthMiddleware::class,
         ]);
