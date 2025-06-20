15ce485194ecd2b06442310415e52aefbbf8adb1
dany214 <dmeza2021@gmail.com>
2025-06-19 20:42:35 -0500

    Refactor CORS configuration: update allowed origins and enable credentials support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append([
-            \Illuminate\Http\Middleware\HandleCors::class, // ⬅️ Añade esta línea
-        ]);
         $middleware->alias([
             'admin' => \App\Http\Middleware\AuthMiddleware::class,
         ]);
